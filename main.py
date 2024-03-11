2489d34bc459f231ad456014c14fe7071e0c413a
L0G1C06 <justgithub70@gmail.com>
2024-03-11 14:11:33 -0300

    add clickable button to download dataset and model folders

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from fastapi import FastAPI, UploadFile, File, Request
-from fastapi.responses import JSONResponse, HTMLResponse
+from fastapi.responses import JSONResponse, HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
 import uvicorn 
 import pandas as pd 
 from sklearn import preprocessing
 from joblib import load 
 import io 
+import os
+import shutil
 
 from versioning import database
 
@@ -24,6 +26,30 @@ def exec_inference(modelFile, testFile):
     score = int(clf_lda.score(X_test, y_test)*100)
     return score
 
+def find_data_path(directory_name: str) -> str:
+    data_versioning_dir = "./data_versioning/" 
+
+    if not os.path.isdir(data_versioning_dir):
+        raise FileNotFoundError("O diretório data_versioning não foi encontrado.")
+
+    for root, dirs, files in os.walk(data_versioning_dir):
+        if directory_name in dirs:
+            return os.path.join(root, directory_name)
+
+    return None
+
+def find_model_path(directory_name: str) -> str:
+    data_versioning_dir = "./model_versioning/"
+
+    if not os.path.isdir(data_versioning_dir):
+        raise FileNotFoundError("O diretório data_versioning não foi encontrado.")
+
+    for root, dirs, files in os.walk(data_versioning_dir):
+        if directory_name in dirs:
+            return os.path.join(root, directory_name)
+
+    return None
+
 @app.get("/")
 async def read_root():
     return {"message": "I'm Alive!"}
@@ -37,22 +63,43 @@ async def run_inference(testFile: UploadFile = File(...)):
     except Exception as e:
         return JSONResponse(content={"error": "Internal Server Error", "error": str(e)}, status_code=500)
     
-@app.get("/query/dataVersioning")
+@app.get("/query/dataVersioning", response_class=HTMLResponse)
 async def query_data_versioning(request: Request):
     try:
         query = database.get_data_table()
-        return templates.TemplateResponse('dataVersioning.html', {'request': request, 'data': query.to_html()})
-        #return JSONResponse(content={"message": f"{query}"}, status_code=200)
+        data = query.to_dict(orient='records') 
+        return templates.TemplateResponse('dataVersioning.html', {'request': request, 'data': data})
     except Exception as e:
         return JSONResponse(content={"error": "Internal Server Error", "error": str(e)}, status_code=500)
     
-@app.get("/query/modelVersioning")
+@app.get("/query/modelVersioning", response_class=HTMLResponse)
 async def query_model_versioning(request: Request):
     try:
         query = database.get_model_table()
-        return templates.TemplateResponse('modelVersioning.html', {'request': request, 'data': query.to_html()})
+        data = query.to_dict(orient='records')
+        return templates.TemplateResponse('modelVersioning.html', {'request': request, 'data': data})
     except Exception as e:
         return JSONResponse(content={"error": "Internal Server Error", "error": str(e)}, status_code=500)
+    
+@app.get("/download/data/{directory_name}/")
+async def download_data_directory(directory_name: str):
+    directory_path = find_data_path(directory_name)
+    if directory_path:
+        zip_file = f"./data_versioning/{directory_name}.zip"
+        shutil.make_archive(directory_path, 'zip', directory_path)
+        return FileResponse(zip_file, media_type='application/zip', filename=f"{directory_name}.zip")
+    else:
+        return {"error": "Diretório não encontrado"}
+    
+@app.get("/download/model/{directory_name}/")
+async def download_data_directory(directory_name: str):
+    directory_path = find_model_path(directory_name)
+    if directory_path:
+        zip_file = f"./model_versioning/{directory_name}.zip"
+        shutil.make_archive(directory_path, 'zip', directory_path)
+        return FileResponse(zip_file, media_type='application/zip', filename=f"{directory_name}.zip")
+    else:
+        return {"error": "Diretório não encontrado"}
     
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8001)
