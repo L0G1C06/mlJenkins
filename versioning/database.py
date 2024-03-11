ee83892d6c036bf1b2d6913b9be0ca5d3d86845b
L0G1C06 <justgithub70@gmail.com>
2024-03-11 14:26:23 -0300

    solve path error

--- versioning/database.py
+++ versioning/database.py
@@ -5,8 +5,8 @@ from tinydb import TinyDB, Query
 import pandas as pd 
 
 db = TinyDB('data_hashes.json')
-conn_data = sqlite3.connect("./versioning/data.db")
-conn_model = sqlite3.connect("./versioning/model.db")
+conn_data = sqlite3.connect("/versioning/data.db")
+conn_model = sqlite3.connect("/versioning/model.db")
 cur_data = conn_data.cursor()
 cur_model = conn_model.cursor()
 
